e42a845a9abe05dc386ffaa4a427b3313993f5fa
Sahil Chopra <sahil.chopra@kite.work>
2019-07-10 13:56:15 +0530

    ModelTests.kt, Code refactored.

--- app/src/test/java/com/demo/popularmovies/ModelTests.kt
+++ app/src/test/java/com/demo/popularmovies/ModelTests.kt
@@ -1,28 +1,44 @@
 package com.demo.popularmovies
 
 import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 
 class ModelTests {
-  @Test
-  fun `User should see the list of movies when UI is first created`() {
-    // Setup
-    val moviesList = listOf<Movie>(
+  lateinit var lifecycleEvents: PublishSubject<MviLifecycle>
+  lateinit var repository: CachedRepository
+  lateinit var previousStates: BehaviorSubject<MoviesState>
+  lateinit var observer: TestObserver<MoviesState>
+  lateinit var moviesList: List<Movie>
+
+  @Before
+  fun setup() {
+    lifecycleEvents = PublishSubject.create()
+    repository = Mockito.mock(CachedRepository::class.java)
+    previousStates = BehaviorSubject.create()
+
+    moviesList = listOf<Movie>(
         Movie(1, "Dhoni", "Movie on player"),
         Movie(2, "Deadpool", "Action comedy dark comedy"),
         Movie(3, "Avengers", "Sci-Fi")
     )
 
+    observer = MoviesModel
+        .bind(lifecycleEvents, repository, previousStates)
+        .doOnNext { previousStates.onNext(it) }
+        .test()
+  }
+
+  @Test
+  fun `User should see the list of movies when UI is first created`() {
+    // Setup
     val loadingState = MoviesState.INITIAL
     val stateWithData = MoviesState(moviesList, FetchAction.SUCCESSFUL, null)
-
-    val lifecycle = PublishSubject.create<MviLifecycle>()
-    val repository = Mockito.mock(CachedRepository::class.java)
-    val previousStates = BehaviorSubject.create<MoviesState>()
 
     `when`(repository.getMovies())
         .thenReturn(
@@ -32,13 +48,8 @@ class ModelTests {
             )
         )
 
-    val observer = MoviesModel
-        .bind(lifecycle, repository, previousStates)
-        .doOnNext { it -> previousStates.onNext(it) }
-        .test()
-
     // Act
-    lifecycle.onNext(MviLifecycle.CREATED)
+    lifecycleEvents.onNext(MviLifecycle.CREATED)
 
     // Assert
     observer
@@ -52,17 +63,10 @@ class ModelTests {
 
     val loadingState = MoviesState(emptyList(), FetchAction.IN_FLIGHT, null)
     val errorState = MoviesState(emptyList(), FetchAction.FAILED, errorMsg)
-    val lifecycleEvents = PublishSubject.create<MviLifecycle>()
-    val repository = Mockito.mock(CachedRepository::class.java)
-    val previousStates = BehaviorSubject.create<MoviesState>()
+
     Mockito.`when`(repository.getMovies())
         .thenReturn(Observable.just(FetchEvent(FetchAction.IN_FLIGHT, null, null),
             FetchEvent(FetchAction.FAILED, null, errorMsg)))
-
-    val observer = MoviesModel
-        .bind(lifecycleEvents, repository, previousStates)
-        .doOnNext { previousStates.onNext(it) }
-        .test()
 
     // Act
     lifecycleEvents.onNext(MviLifecycle.CREATED)
@@ -74,22 +78,11 @@ class ModelTests {
   @Test
   fun `User should see the same data on configuration changes`() {
     // Setup
-    val moviesList = listOf<Movie>(
-        Movie(1, "Dhoni", "Movie on player"),
-        Movie(2, "Deadpool", "Action comedy dark comedy"),
-        Movie(3, "Avengers", "Sci-Fi")
-    )
-    val lifecycleEvents = PublishSubject.create<MviLifecycle>()
-    val repository = Mockito.mock(CachedRepository::class.java)
-    val previousStates = BehaviorSubject.create<MoviesState>()
     Mockito
         .`when`(repository.getMovies())
         .thenReturn(Observable.just(FetchEvent(FetchAction.SUCCESSFUL, moviesList, null)
         ))
-    val observer = MoviesModel
-        .bind(lifecycleEvents, repository, previousStates)
-        .doOnNext { previousStates.onNext(it) }
-        .test()
+
     val state = MoviesState(moviesList, FetchAction.SUCCESSFUL, null)
 
     // Act
