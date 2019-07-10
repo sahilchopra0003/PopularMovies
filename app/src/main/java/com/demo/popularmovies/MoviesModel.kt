35886d525b01a41fec21fbfe7101c38a59953390
Sahil Chopra <sahil.chopra@kite.work>
2019-07-10 13:38:14 +0530

    ModelTests.kt, Wrote lifecycle resumed test

--- app/src/main/java/com/demo/popularmovies/MoviesModel.kt
+++ app/src/main/java/com/demo/popularmovies/MoviesModel.kt
@@ -1,18 +1,40 @@
 package com.demo.popularmovies
 
 import io.reactivex.Observable
+import io.reactivex.rxkotlin.withLatestFrom
 
 object MoviesModel {
-    fun bind(
-        lifecycle: Observable<MviLifecycle>,
-        repository: CachedRepository
-    ): Observable<MoviesState> {
-        return lifecycle
-            .filter { it == MviLifecycle.CREATED }
-            .switchMap {
-                repository
-                    .getMovies()
-                    .map { MoviesState(it.result ?: emptyList(), it.fetchAction, it.error) }
-            }
-    }
+  fun bind(
+      lifecycle: Observable<MviLifecycle>,
+      repository: CachedRepository,
+      previousStates: Observable<MoviesState>
+  ): Observable<MoviesState> {
+    return Observable.merge(
+        lifecycleCreatedUsecase(lifecycle, repository),
+        lifecycleResumedUsecase(lifecycle, previousStates)
+    )
+  }
+
+  fun lifecycleCreatedUsecase(
+      lifecycle: Observable<MviLifecycle>,
+      repository: CachedRepository
+  ): Observable<MoviesState> {
+    return lifecycle
+        .filter { it == MviLifecycle.CREATED }
+        .switchMap {
+          repository
+              .getMovies()
+              .map { MoviesState(it.result ?: emptyList(), it.fetchAction, it.error) }
+        }
+  }
+
+  fun lifecycleResumedUsecase(
+      lifecycle: Observable<MviLifecycle>,
+      previousStates: Observable<MoviesState>
+  ): Observable<MoviesState> {
+    return lifecycle
+        .filter { it == MviLifecycle.RESUMED }
+        .withLatestFrom(previousStates)
+        .map { (_, previousState) -> previousState }
+  }
 }
--- app/src/test/java/com/demo/popularmovies/ModelTests.kt
+++ app/src/test/java/com/demo/popularmovies/ModelTests.kt
@@ -1,6 +1,7 @@
 package com.demo.popularmovies
 
 import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 import org.junit.Test
 import org.mockito.Mockito
@@ -21,6 +22,7 @@ class ModelTests {
 
     val lifecycle = PublishSubject.create<MviLifecycle>()
     val repository = Mockito.mock(CachedRepository::class.java)
+    val previousStates = BehaviorSubject.create<MoviesState>()
 
     `when`(repository.getMovies())
         .thenReturn(
@@ -30,39 +32,71 @@ class ModelTests {
             )
         )
 
-    val observer = MoviesModel.bind(lifecycle, repository)
+    val observer = MoviesModel
+        .bind(lifecycle, repository, previousStates)
+        .doOnNext { it -> previousStates.onNext(it) }
         .test()
 
     // Act
     lifecycle.onNext(MviLifecycle.CREATED)
 
     // Assert
-
     observer
         .assertValues(loadingState, stateWithData)
   }
 
   @Test
   fun `User should see the error when no internet`() {
-    //Setup
+    // Setup
     val errorMsg = "Oops! Something went wrong."
 
     val loadingState = MoviesState(emptyList(), FetchAction.IN_FLIGHT, null)
     val errorState = MoviesState(emptyList(), FetchAction.FAILED, errorMsg)
     val lifecycleEvents = PublishSubject.create<MviLifecycle>()
     val repository = Mockito.mock(CachedRepository::class.java)
+    val previousStates = BehaviorSubject.create<MoviesState>()
     Mockito.`when`(repository.getMovies())
         .thenReturn(Observable.just(FetchEvent(FetchAction.IN_FLIGHT, null, null),
             FetchEvent(FetchAction.FAILED, null, errorMsg)))
 
     val observer = MoviesModel
-        .bind(lifecycleEvents, repository)
+        .bind(lifecycleEvents, repository, previousStates)
+        .doOnNext { previousStates.onNext(it) }
         .test()
 
-    //Act
+    // Act
     lifecycleEvents.onNext(MviLifecycle.CREATED)
 
-    //Assert
+    // Assert
     observer.assertValues(loadingState, errorState)
+  }
+
+  @Test
+  fun `User should see the same data on configuration changes`() {
+    // Setup
+    val moviesList = listOf<Movie>(
+        Movie(1, "Dhoni", "Movie on player"),
+        Movie(2, "Deadpool", "Action comedy dark comedy"),
+        Movie(3, "Avengers", "Sci-Fi")
+    )
+    val lifecycleEvents = PublishSubject.create<MviLifecycle>()
+    val repository = Mockito.mock(CachedRepository::class.java)
+    val previousStates = BehaviorSubject.create<MoviesState>()
+    Mockito
+        .`when`(repository.getMovies())
+        .thenReturn(Observable.just(FetchEvent(FetchAction.SUCCESSFUL, moviesList, null)
+        ))
+    val observer = MoviesModel
+        .bind(lifecycleEvents, repository, previousStates)
+        .doOnNext { previousStates.onNext(it) }
+        .test()
+    val state = MoviesState(moviesList, FetchAction.SUCCESSFUL, null)
+
+    // Act
+    lifecycleEvents.onNext(MviLifecycle.CREATED)
+    lifecycleEvents.onNext(MviLifecycle.RESUMED)
+
+    // Assert
+    observer.assertValues(state, state)
   }
 }
