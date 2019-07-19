82c42be81656c22208b7b5caebe672e7ab5aa0d4
Sahil Chopra <sahil.chopra@kite.work>
2019-07-19 14:58:00 +0530

    MoviesModel.kt, Code Reformatted.

--- app/src/main/java/com/demo/popularmovies/MoviesModel.kt
+++ app/src/main/java/com/demo/popularmovies/MoviesModel.kt
@@ -4,51 +4,53 @@ import io.reactivex.Observable
 import io.reactivex.rxkotlin.withLatestFrom
 
 object MoviesModel {
-  fun bind(
-      lifecycle: Observable<MviLifecycle>,
-      userIntentions: MoviesIntention,
-      repository: CachedRepository,
-      previousStates: Observable<MoviesState>
-  ): Observable<MoviesState> {
-    return Observable.merge(
-        lifecycleCreatedUsecase(lifecycle, repository),
-        lifecycleResumedUsecase(lifecycle, previousStates),
-        refreshListUseCase(userIntentions, repository)
-    )
-  }
+    fun bind(
+        lifecycle: Observable<MviLifecycle>,
+        userIntentions: MoviesIntention,
+        repository: CachedRepository,
+        previousStates: Observable<MoviesState>
+    ): Observable<MoviesState> {
+        return Observable.merge(
+            lifecycleCreatedUsecase(lifecycle, repository),
+            lifecycleResumedUsecase(lifecycle, previousStates),
+            refreshListUseCase(userIntentions, repository)
+        )
+    }
 
-  fun lifecycleCreatedUsecase(
-      lifecycle: Observable<MviLifecycle>,
-      repository: CachedRepository
-  ): Observable<MoviesState> {
-    return lifecycle
-        .filter { it == MviLifecycle.CREATED }
-        .switchMap {
-          repository
-              .getMovies()
-              .map { MoviesState(it.result ?: emptyList(), it.fetchAction, it.error) }
-        }
-  }
+    fun lifecycleCreatedUsecase(
+        lifecycle: Observable<MviLifecycle>,
+        repository: CachedRepository
+    ): Observable<MoviesState> {
+        return lifecycle
+            .filter { it == MviLifecycle.CREATED }
+            .switchMap {
+                repository
+                    .getMovies()
+                    .map { MoviesState(it.result ?: emptyList(), it.fetchAction, it.error) }
+                    .onErrorReturn { MoviesState(emptyList(), FetchAction.FAILED, it.message) }
+            }
+    }
 
-  fun lifecycleResumedUsecase(
-      lifecycle: Observable<MviLifecycle>,
-      previousStates: Observable<MoviesState>
-  ): Observable<MoviesState> {
-    return lifecycle
-        .filter { it == MviLifecycle.RESUMED }
-        .withLatestFrom(previousStates)
-        .map { (_, previousState) -> previousState }
-  }
+    fun lifecycleResumedUsecase(
+        lifecycle: Observable<MviLifecycle>,
+        previousStates: Observable<MoviesState>
+    ): Observable<MoviesState> {
+        return lifecycle
+            .filter { it == MviLifecycle.RESUMED }
+            .withLatestFrom(previousStates)
+            .map { (_, previousState) -> previousState }
+    }
 
-  fun refreshListUseCase(
-      userIntentions: MoviesIntention,
-      repository: CachedRepository
-  ): Observable<MoviesState> {
-    return userIntentions.refreshEvents
-        .switchMap {
-          repository
-              .getMovies()
-              .map { MoviesState(it.result ?: emptyList(), it.fetchAction, it.error) }
-        }
-  }
+    fun refreshListUseCase(
+        userIntentions: MoviesIntention,
+        repository: CachedRepository
+    ): Observable<MoviesState> {
+        return userIntentions.refreshEvents
+            .switchMap {
+                repository
+                    .getMovies()
+                    .map { MoviesState(it.result ?: emptyList(), it.fetchAction, it.error) }
+                    .onErrorReturn { MoviesState(emptyList(), FetchAction.FAILED, it.message) }
+            }
+    }
 }
